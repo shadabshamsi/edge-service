c9633aae5ec9d66cdac0a551f44813681381ed36
Shadab Shamsi <shadablaptop@gmail.com>
2024-05-04 17:45:27 +0530

    completed chapter 12

--- src/main/java/com/shadabshamsi/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/shadabshamsi/edgeservice/config/RateLimiterConfig.java
@@ -7,13 +7,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
+import java.security.Principal;
+
 @Configuration
 public class RateLimiterConfig {
 
     @Bean
     public KeyResolver keyResolver() {
 //        return exchange -> exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
-        return exchange -> Mono.just("anonymous");
+//        return exchange -> Mono.just("anonymous");
+        return exchange -> exchange.getPrincipal().map(Principal::getName).defaultIfEmpty("anonymous");
     }
 
 }
